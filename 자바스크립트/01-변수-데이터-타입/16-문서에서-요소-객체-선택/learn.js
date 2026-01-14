d15bf63af6627e2088a567941b3dec707f70bb32
saminjae <smj123432@gmail.com>
2026-01-14 16:42:41 +0900

    학습: 문서의 요소 클래스 이름 제어 (추가add , 제거 remove, 교체 replace)

--- 자바스크립트/01-변수-데이터-타입/16-문서에서-요소-객체-선택/learn.js
+++ 자바스크립트/01-변수-데이터-타입/16-문서에서-요소-객체-선택/learn.js
@@ -6,63 +6,140 @@
 // * element.querySelector() : 특정 요소 내부에서만 탐색합니다.
 // --------------------------------------------------------------------------
 
-// 아주 오래 전 웹 브라우저 세상에서 사용했던 오래된 방법(비표준)
-console.log(document.all);
+// 아주 오래 전 웹 브라우저 세상에서 사용했던 오래된 방법 (⚠️ 비표준: Microsoft사의 IE 브라우저)
+// console.log(document.all) // HTMLAllCollection []
 
-// 후에 시간이 흘러 사람들은 표준의 필요성을 절실히 느끼기 시작
-// 표준 API를 만들기 시작했는데...
-console.log(document.getElementById('p'));
-// 근데 여러 요소들을 수집하는 것 말고, 문서에서 고유한 id 속성을 가진 단 하나의 요소를 선택하는 방법이 필요
-console.log(document.getElementById('content'));
-console.log(document.getElementById('article'));
+// 그리고 시간이 흘러 사람들은 표준의 필요성을 절실히 느끼기 시작
+// 표준 API를 만들기 시작했는데... (✅ 표준)
+// console.log(document.getElementsByTagName('p')) // HTMLCollection []
+
+// 브라우저에서 하는 일
+// * HTML 파서(해석 프로그램)
+//   - *.html 파일 파싱(해석) -> DOM 생성
+// * JavaScript 엔진
+//   - *.js 파일 처리 -> DOM에 접근/조작
+//   - *.js 파일 처리하는 동안 HTML 파서는 대기 상태가 됨. (동기식, 일을 안하고 대기 중...)
+
+// 그런데 여러 요소들을 수집하는 것 말고,
+// 문서에서 고유한 id 속성을 가진 단 하나의 요소를 선택하는 방법이 필요 (✅ 표준)
+// console.log(document.getElementById('content')) // Element (찾은 경우) 또는 null (못찾은 경우)
+
+// <html> 요소(객체) 선택
+// console.log(document.documentElement)
+// console.log(document.querySelector('html'))
+
+// <head> 요소(객체) 선택
+// console.log(document.head)
+// console.log(document.querySelector('head'))
+
+// <title> 요소(객체) 선택
+// console.log(document.querySelector('title'))
+
+// <meta /> 요소(객체) 선택
+// console.log(document.querySelector('meta'))
+
+// <body> 요소(객체) 선택
+// console.log(document.body)
+// console.log(document.querySelector('body'))
+
+// <article> 요소(객체) 선택
+// console.log(document.querySelector('article'))
+// console.log(document.querySelector('#content'))
 
 // --------------------------------------------------------------------------
 // document.querySelector - 전체 문서 탐색
-// --------------------------------------------------------------------------
+// -------------------------------------------------------------------------
 
 // 1. ID 선택자를 사용하여 '#chapter' 요소를 선택하고 콘솔에 출력하세요.
+let chapterElement = document.getElementById("chapter");
+chapterElement = document.querySelector("#chapter");
+// console.log(chapterElement) // Element 또는 null
 
+// 조건 처리 (문서에 요소가 존재하는지 검토)
+// chapterElement || console.warn('#chapter 요소가 문서에 없습니다.')
+// if (!chapterElement) {
+checkElementWarn(chapterElement, "#chapter");
 
 // 2. 클래스 선택자를 사용하여 '.sr-only' 요소를 선택하고 콘솔에 출력하세요.
+const screenReaderOnlyElement = document.querySelector(".sr-only");
+// console.log(screenReaderOnlyElement)
 
+checkElementWarn(screenReaderOnlyElement, ".sr-only");
 
 // 3. 속성 선택자를 사용하여 title에 'Model'이 포함된([title*="Model"]) 요소를 선택하세요.
+const modelElement = document.querySelector('[title*="Model"]');
+// console.log(modelElement)
 
+checkElementWarn(modelElement, '[title*="Model"]');
+
+// 반복되는 코드를 재사용하기 위해 함수를 작성하기로 했다.
+// 함수(기능)에 걸맞는 이름을 작성하기로 했다.
+// 기능: 문서에 요소가 존재하는지 확인한 후, 존재하지 않을 경우 콘솔에 경고하기로 했다.
+// 목적: 현재 어떤 상황의 문제가 발생했는지 개발자에게 알리고자 한다.
+function checkElementWarn(element, selector) {
+  // 요소가 문서에 존재하는지 검증
+  // 문서에 요소가 없다면? 콘솔에 경고
+  if (element === null) {
+    console.warn(selector + " 선택자로 문서에서 요소를 찾을 수 없습니다.");
+  }
+}
 
 // 설명:
 // querySelector는 CSS와 동일한 선택자 방식을 사용하여 요소를 선택합니다.
 // 일치하는 대상이 여러 개여도 '가장 먼저 발견된 첫 번째' 요소만 반환합니다.
 
+// const firstPElement = document.querySelector('p')
+// console.log(firstPElement) // 문서에 <p> 요소가 2개 있어도 첫 번째 것만 반환
 
 // --------------------------------------------------------------------------
 // element.querySelector - 특정 범위 내 탐색
 // --------------------------------------------------------------------------
 
-// 1. 먼저 부모 요소인 '.musicians'를 찾아 변수 musicianList에 할당하세요.
+// 문서의 모든 <li> 요소 중 첫 번째 매칭되는 요소 반환 (없을 경우, null 반환)
+// CSS 선택자가 복잡한 구조일 수록 문서의 대상(요소)을 찾는데 더 많은 시간을 소요 (권장 ❌)
+// const liElement = document.querySelector('.musicians > li:first-child')
+// console.log(liElement)
 
+// 1. 먼저 부모 요소인 '.musicians'를 찾아 변수 musicianList에 할당하세요.
+const musicianList = document.querySelector(".musicians");
+checkElementWarn(musicianList, ".musicians");
 
 // 2. musicianList 변수(이미 선택된 요소) 내부에서만 'li' 요소를 찾아 출력하세요.
-
+const firstMusician = musicianList.querySelector("li");
+checkElementWarn(firstMusician, "li:fisrt-child");
+// console.log(firstMusician);
 
 // 설명:
 // 특정 서가(부모 요소)를 지정하고 그 안에서 책(자식 요소)을 찾는 방식입니다.
 // 코드의 의도가 명확해지고, 다른 구역의 요소와 충돌할 버그를 예방하며, 성능 면에서도 효율적입니다.
 
-
+const emails = document.getElementsByName("email");
+// console.log(emails);
 // --------------------------------------------------------------------------
 // 그 밖의 요소 선택 API (참고)
 // --------------------------------------------------------------------------
+const heros = document.getElementById('exorcist-characters')
+console.log(heros);
+// const heroos = heros.querySelector('li')
+// console.log(heroos);
 
+// 요한
+// 요한 태그
+let yohan = null
+yohan = heros.getElementsByName('li').item(0)
+// 요한 클래스
+yohan = heros.querySelector('li')
+// 요한 속성 이름 값으로 찾기
+yohan = heros.getElementsByClassName('character').item(0)
+console.log(yohan);
+//const heros = document.querySelector('#exorcist-characters')
 // 1. getElementById()를 사용하여 '#chapter' 요소를 선택해 보세요.
-
 
 // 2. getElementsByTagName()을 사용하여 모든 'li' 태그를 선택해 보세요.
 
-
 // 설명:
-// querySelector 이전에 사용되던 고전적인 주문들입니다. 
+// querySelector 이전에 사용되던 고전적인 주문들입니다.
 // ID 선택 시 getElementById는 매우 빠르지만, 유연함은 querySelector가 더 뛰어납니다.
-
 
 // --------------------------------------------------------------------------
 // 핵심 요약!
